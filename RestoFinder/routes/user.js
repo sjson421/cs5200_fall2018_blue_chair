b4710ab0791e7a1e590aba766d3f1dd5c8443afe
Tejas Parab <tejasparab890@gmail.com>
2018-11-30 23:08:30 -0500

    added bcryptjs and jsonwebtoken packages, minor changes to UserSchema

--- RestoFinder/routes/user.js
+++ RestoFinder/routes/user.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const router = express.Router();
+
+const User = require('../data/models/User.schema.server');
+
+
 // Get user dao here
 router.get("/test", (req, res) => res.json({ msg: "User works" }));
+
+/// register User
+router.post('/', (req, res) => {
+
+})
+
 
 module.exports = router;
