cf82e5c758611bebeff7a10da7fab2ba85a040cd
Jae Son <sjson421@gmail.com>
2018-12-08 18:35:10 -0500

    register progress

--- RestoFinder/public/services/user.service.js
+++ RestoFinder/public/services/user.service.js
@@ -7,7 +7,9 @@
         var baseUrl = "http://localhost:5000/api/user"
         var api = {
             getAllUsers: getAllUsers,
-            getUser: getUser
+            getUser: getUser,
+            setUser: setUser,
+            register: register
         };
         return api;
 
@@ -23,7 +25,6 @@
 
         }
         function register(user) {
-          //  user.
         }
     }
 })();
